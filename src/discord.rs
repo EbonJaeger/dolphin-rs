47e7878f8aa58fd666275f946fee4ff48f2dfe06
Evan Maddock <maddock.evan@vivaldi.net>
2020-11-21 16:32:34 -0500

    Minor cleanup of the code I just wrote
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/discord.rs
+++ src/discord.rs
@@ -94,7 +94,7 @@ impl Handler {
     /// Performs some string replacements for mentions and escapes quotes on
     /// messages that are to be sent to the Minecraft server.
     ///
-    async fn sanitize_message(&self, ctx: &Context, msg: &Message) -> Result<String, DolphinError> {
+    async fn sanitize_message(&self, ctx: &Context, msg: &Message) -> String {
         let content = msg.content.clone();
         let mut sanitized = msg.content.clone();
 
@@ -110,7 +110,10 @@ impl Handler {
             let id = match chan_mention.get(2..chan_mention.len() - 1) {
                 Some(id) => match id.parse::<u64>() {
                     Ok(num) => num,
-                    Err(e) => return Err(DolphinError::Parse(e)),
+                    Err(e) => {
+                        debug!("handler:sanitize_message: Error parsing channel id: {}. This might not have been intended to be a channel mention", e);
+                        continue;
+                    }
                 },
                 None => continue,
             };
@@ -126,22 +129,19 @@ impl Handler {
                 }
             };
 
-            sanitized = sanitized.replace(
-                format!("<#{}>", id).as_str(),
-                format!("#{}", channel.name()).as_str(),
-            );
+            sanitized = sanitized.replace(chan_mention, format!("#{}", channel.name()).as_str());
         }
 
-        for role_mention in &msg.mention_roles {
-            let role = match role_mention.to_role_cached(&ctx.cache).await {
+        for role_id in &msg.mention_roles {
+            let role = match role_id.to_role_cached(&ctx.cache).await {
                 Some(role) => role,
                 None => {
-                    warn!("handler:sanitize_message: Role was mentioned but not found in the cache: {}", role_mention);
+                    warn!("handler:sanitize_message: Role was mentioned but not found in the cache: {}", role_id);
                     continue;
                 }
             };
             sanitized = sanitized.replace(
-                role_mention.mention().as_str(),
+                role_id.mention().as_str(),
                 format!("@{}", role.name).as_str(),
             );
         }
@@ -153,9 +153,8 @@ impl Handler {
             );
         }
 
-        let sanitized = sanitized.replace("\"", "\\\"");
-
-        Ok(sanitized)
+        // Escape double quotes
+        sanitized.replace("\"", "\\\"")
     }
 
     ///
@@ -215,16 +214,7 @@ impl EventHandler for Handler {
         }
 
         debug!("event_handler:message: received a message from Discord");
-        let content = match self.sanitize_message(&ctx, &msg).await {
-            Ok(string) => string,
-            Err(e) => {
-                error!(
-                    "event_handler:message: Error sanitizing message content: {}",
-                    e
-                );
-                return;
-            }
-        };
+        let content = self.sanitize_message(&ctx, &msg).await;
 
         // Send a separate message for each line
         let lines = content.split("\n");
