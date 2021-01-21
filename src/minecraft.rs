281ea8b33d01f68b34d6b574109fc0dc34297ccd
Evan Maddock <maddock.evan@vivaldi.net>
2021-01-21 15:22:55 -0500

    Add tests for the Minecraft message parser
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/minecraft.rs
+++ src/minecraft.rs
@@ -172,15 +172,207 @@ fn trim_prefix(line: &str) -> Option<&str> {
     }
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
 pub enum Source {
     Player,
     Server,
 }
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct MinecraftMessage {
     pub name: String,
     pub content: String,
     pub source: Source,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::minecraft::MessageParser;
+    use crate::minecraft::MinecraftMessage;
+    use crate::minecraft::Source;
+
+    #[test]
+    fn parse_vanilla_chat_line() {
+        // Given
+        let input =
+            String::from("[12:32:45] [Server thread/INFO]: <TestUser> Sending a chat message");
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::from("TestUser"),
+            content: String::from("Sending a chat message"),
+            source: Source::Player,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse chat message"),
+        }
+    }
+
+    #[test]
+    fn parse_non_vanilla_chat_line() {
+        // Given
+        let input = String::from(
+            "[12:32:45] [Async Chat Thread - #0/INFO]: <TestUser> Sending a chat message",
+        );
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::from("TestUser"),
+            content: String::from("Sending a chat message"),
+            source: Source::Player,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse non-vanilla chat message"),
+        }
+    }
+
+    #[test]
+    fn parse_join_line() {
+        // Given
+        let input = String::from("[12:32:45] [Server thread/INFO]: TestUser joined the game");
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from("TestUser joined the game"),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse join message"),
+        }
+    }
+
+    #[test]
+    fn parse_leave_line() {
+        // Given
+        let input = String::from("[12:32:45] [Server thread/INFO]: TestUser left the game");
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from("TestUser left the game"),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse leave message"),
+        }
+    }
+
+    #[test]
+    fn parse_advancement_line() {
+        // Given
+        let input = String::from(
+            "[12:32:45] [Server thread/INFO]: TestUser has made the advancement [MonsterHunter]",
+        );
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from(
+                ":partying_face: TestUser has made the advancement [MonsterHunter]",
+            ),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse advancement message"),
+        }
+    }
+
+    #[test]
+    fn parse_advancement2_line() {
+        // Given
+        let input = String::from(
+            "[12:32:45] [Server thread/INFO]: TestUser has completed the challenge [MonsterHunter]",
+        );
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from(
+                ":partying_face: TestUser has completed the challenge [MonsterHunter]",
+            ),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse challenge message"),
+        }
+    }
+
+    #[test]
+    fn parse_server_start_line() {
+        // Given
+        let input = String::from(
+            "[12:32:45] [Server thread/INFO]: Done (21.3242s)! For help, type \"help\"",
+        );
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from(":white_check_mark: Server has started"),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse server started message"),
+        }
+    }
+
+    #[test]
+    fn parse_server_stop_line() {
+        // Given
+        let input = String::from("[12:32:45] [Server thread/INFO]: Stopping the server");
+        let parser = MessageParser::new(vec![]);
+        let expected = MinecraftMessage {
+            name: String::new(),
+            content: String::from(":x: Server is shutting down"),
+            source: Source::Server,
+        };
+
+        // When/Then
+        match parser.parse_line(&input) {
+            Some(msg) => {
+                assert_eq!(msg, expected);
+            }
+            None => panic!("failed to parse server stopped message"),
+        }
+    }
+
+    #[test]
+    fn parser_ignore_villager_death_message() {
+        // Given
+        let input = String::from("[12:32:45] [Server thread/INFO]: Villager axw['Villager'/85, l='world', x=-147.30, y=57.00, z=-190.70] died, message: 'Villager was squished too much'");
+        let parser = MessageParser::new(vec![]);
+
+        // When/Then
+        if let Some(_) = parser.parse_line(&input) {
+            panic!("parsed a message when the line should be ignored")
+        }
+    }
 }
