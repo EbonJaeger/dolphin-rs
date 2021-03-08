8af64faabbbdc9feb59b6ac6e9454e891be072f8
Evan Maddock <maddock.evan@vivaldi.net>
2021-03-07 20:41:32 -0500

    Finally move the sending to Discord functions where they belong
    I don't know what this does for the end user besides maybe better performance/less overhead because we're no longer passing messages over channels.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/discord.rs
+++ src/discord.rs
@@ -2,15 +2,13 @@ use crate::config::RootConfig;
 use crate::errors::Error;
 use crate::listener::{Listener, LogTailer, Webserver};
 use crate::markdown;
-use crate::minecraft::{MinecraftMessage, Source};
-use fancy_regex::Regex;
 use rcon::Connection;
 use serenity::{
     async_trait,
     model::{
         channel::Message,
         gateway::{Activity, Ready},
-        id::{ChannelId, GuildId},
+        id::GuildId,
     },
     prelude::*,
     utils::parse_channel,
@@ -19,8 +17,7 @@ use std::sync::{
     atomic::{AtomicBool, AtomicU64, Ordering},
     Arc,
 };
-use tokio::sync::mpsc;
-use tracing::{debug, error, info, warn};
+use tracing::{debug, error, info};
 
 const MAX_LINE_LENGTH: usize = 100;
 
@@ -142,38 +139,23 @@ impl EventHandler for Handler {
 
         // Only do stuff if we're not already running
         if !self.is_watching.load(Ordering::Relaxed) {
-            let (tx, mut rx) = mpsc::channel(100);
-
             // Create our listener and start waiting for messages
             if cfg.enable_webserver() {
                 let port = cfg.get_webserver_port();
                 tokio::spawn(async move {
                     let listener = Webserver::new(port);
-                    listener.listen(tx).await;
+                    listener
+                        .listen(ctx.clone(), cfg.clone(), guild_id.clone())
+                        .await;
                 });
             } else {
                 let log_tailer = LogTailer::new(log_path.to_string(), cfg.get_death_keywords());
-                tokio::spawn(async move { log_tailer.listen(tx).await });
+                tokio::spawn(async move {
+                    log_tailer
+                        .listen(ctx.clone(), cfg.clone(), guild_id.clone())
+                        .await
+                });
             }
-
-            // Spawn a task to wait for messages and send them to Discord
-            tokio::spawn(async move {
-                while let Some(message) = rx.recv().await {
-                    if let Err(e) = send_to_discord(
-                        Arc::clone(&ctx),
-                        Arc::clone(&cfg),
-                        Arc::clone(&guild_id),
-                        message,
-                    )
-                    .await
-                    {
-                        error!(
-                            "discord:handler: unable to send a message to Discord: {}",
-                            e
-                        );
-                    }
-                }
-            });
         }
 
         self.is_watching.swap(true, Ordering::Relaxed);
@@ -323,193 +305,8 @@ fn truncate_lines(lines: Vec<String>) -> Vec<String> {
     truncated
 }
 
-///
-/// Send a message from a Minecraft server to a configured Discord channel, either
-/// directly or via a webhook integration.
-///
-/// # Errors
-///
-/// Returns a `serenity::Error` if a message is unable to be sent to the channel or the webhook.
-///
-async fn send_to_discord(
-    ctx: Arc<Context>,
-    cfg: Arc<RootConfig>,
-    guild_id: Arc<GuildId>,
-    message: MinecraftMessage,
-) -> Result<(), Error> {
-    debug!(
-        "dolphin:send_to_discord: received a message from a Minecraft instance: {:?}",
-        message
-    );
-
-    // Get the correct name to use
-    let name = match message.source {
-        Source::Player => message.name.clone(),
-        Source::Server => ctx.cache.current_user().await.name,
-    };
-
-    let mut content = message.content.clone();
-    if cfg.mentions_allowed() {
-        content = replace_mentions(Arc::clone(&ctx), guild_id, content).await;
-    }
-
-    let message = MinecraftMessage {
-        name: name.clone(),
-        content,
-        source: message.source,
-    };
-
-    // Check if we should use a webhook to post the message
-    if cfg.webhook_enabled() {
-        let url = &cfg.webhook_url();
-
-        if let Err(e) = post_to_webhook(Arc::clone(&ctx), message, url).await {
-            return Err(e);
-        }
-    } else {
-        // Send the message to the channel
-        let final_msg = match message.source {
-            Source::Player => format!("**{}**: {}", message.name, message.content),
-            Source::Server => message.content,
-        };
-
-        if let Err(e) = ChannelId(cfg.get_channel_id()).say(&ctx, final_msg).await {
-            return Err(Error::Discord(e));
-        }
-    }
-
-    Ok(())
-}
-
-///
-/// Looks for instances of mentions in a message and attempts
-/// to replace that text with an actual Discord `@mention` (or
-/// `#channel` in the case of a channel).
-///
-/// It tries to match names using the full name and, in the
-/// case of users, optionally their  descriptor. This works
-/// for names that have spaces in them, and really probably
-/// anything else.
-///
-async fn replace_mentions(ctx: Arc<Context>, guild_id: Arc<GuildId>, message: String) -> String {
-    let mut ret = message.clone();
-
-    if let Some(guild) = ctx.cache.guild(*guild_id).await {
-        let mut found_start = false;
-        let mut start = 0;
-        let mut end = 0;
-        let cloned = ret.clone();
-
-        for (i, c) in cloned.char_indices() {
-            if !found_start && (c == '@' || c == '#') {
-                found_start = true;
-                start = i;
-            } else if found_start && c == '#' {
-                end = i + 5;
-            } else if found_start && c == ' ' {
-                end = i;
-            } else if found_start && cloned.len() == i + 1 {
-                end = i + 1;
-            }
-
-            // Check to see if we have a mention
-            if found_start && end > 0 {
-                if let Some(mention) = cloned.get(start..end) {
-                    let name = &mention[1..];
-                    if let Some(member) = guild.member_named(name) {
-                        ret = ret.replace(mention, &member.mention().to_string());
-                    } else if let Some(role) = guild.role_by_name(name) {
-                        ret = ret.replace(mention, &role.mention().to_string());
-                    } else if let Some(id) = guild.channel_id_from_name(ctx.clone(), name).await {
-                        if let Some(channel) = ctx.cache.channel(id).await {
-                            ret = ret.replace(mention, &channel.mention().to_string());
-                        }
-                    } else {
-                        continue;
-                    }
-
-                    // If we got here, we found a mention, so reset everything
-                    start = 0;
-                    end = 0;
-                    found_start = false;
-                }
-            }
-        }
-    } else {
-        warn!("Unable to get the Guild from the cache: Guild not found");
-    }
-
-    ret
-}
-
-///
-/// Post a message to the configured Discord webhook.
-///
-async fn post_to_webhook(
-    ctx: Arc<Context>,
-    message: MinecraftMessage,
-    url: &str,
-) -> Result<(), Error> {
-    // Split the url into the webhook id an token
-    let parts = match split_webhook_url(url) {
-        Some(parts) => parts,
-        None => return Err(Error::Other("invalid webhook url")),
-    };
-
-    // Get the webhook using the id and token
-    let webhook = ctx.http.get_webhook_with_token(parts.0, parts.1).await?;
-
-    // Get the avatar URL
-    let avatar_url = match message.source {
-        Source::Player => format!("https://minotar.net/helm/{}/256.png", message.name.clone()),
-        // TODO: Do something better than a blind unwrap() here
-        Source::Server => ctx.cache.current_user().await.avatar_url().unwrap(),
-    };
-
-    // Post to the webhook
-    if let Err(e) = webhook
-        .execute(&ctx.http, false, |w| {
-            w.avatar_url(avatar_url)
-                .username(message.name)
-                .content(message.content)
-        })
-        .await
-    {
-        return Err(Error::Discord(e));
-    }
-
-    Ok(())
-}
-
-///
-/// Use Regex to split the configured webhook URL into an ID and a token.
-///
-fn split_webhook_url(url: &str) -> Option<(u64, &str)> {
-    // Only compile the regex once, since this is expensive
-    lazy_static! {
-        static ref WEBHOOK_REGEX: Regex =
-            Regex::new(r"^https://discord.com/api/webhooks/(?P<id>.*)/(?P<token>.*)$").unwrap();
-    }
-
-    let mut ret = None;
-
-    if let Ok(Some(captures)) = WEBHOOK_REGEX.captures(&url) {
-        if captures.len() != 3 {
-            return None;
-        }
-
-        let id = captures.name("id").unwrap().as_str();
-        if let Ok(id) = id.parse::<u64>() {
-            ret = Some((id, captures.name("token").unwrap().as_str()));
-        }
-    }
-
-    ret
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::discord::split_webhook_url;
     use crate::discord::truncate_lines;
 
     #[test]
@@ -536,31 +333,5 @@ mod tests {
 
         // Then
         assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn parse_parts_from_webhook_url() {
-        // Given
-        let input = String::from("https://discord.com/api/webhooks/12345/67890");
-
-        // When/Then
-        match split_webhook_url(&input) {
-            Some((id, token)) => {
-                assert_eq!(id, 12345);
-                assert_eq!(token, String::from("67890"));
-            }
-            None => panic!("failed to parse Discord webhook url"),
-        }
-    }
-
-    #[test]
-    fn parse_non_webhook_url() {
-        // Given
-        let input = String::from("https://example.com");
-
-        // When/Then
-        if let Some(_) = split_webhook_url(&input) {
-            panic!("webhook split returned something when it should have returned None");
-        }
     }
 }
--- src/listener.rs
+++ src/listener.rs
@@ -1,38 +1,36 @@
-use crate::minecraft::{MessageParser, MinecraftMessage};
+use std::sync::Arc;
+
+use crate::{
+    config::RootConfig,
+    errors::Error,
+    minecraft::{MessageParser, MinecraftMessage, Source},
+};
+use fancy_regex::Regex;
 use linemux::MuxedLines;
-use serenity::async_trait;
-use serenity::futures::StreamExt;
-use tokio::sync::mpsc::Sender;
-use tracing::{error, info};
+use serenity::{async_trait, client::Context, model::id::ChannelId, prelude::Mentionable};
+use serenity::{futures::StreamExt, model::id::GuildId};
+use tracing::{debug, error, info, warn};
 use warp::Filter;
 
-///
 /// A Listener listens or watches for new messages from a Minecraft instance,
-/// depending on the implementation. Listeners require a channel to pass messages
-/// through so they can be sent to Discord.
-///
+/// depending on the implementation.
 #[async_trait]
 pub trait Listener {
-    ///
     /// Begin listening for messages from Minecraft. Usually you'll want to
     /// call this from an async thread so it doesn't block the rest of the
     /// program.
-    ///
-    async fn listen(&self, tx: Sender<MinecraftMessage>);
+    async fn listen(&self, ctx: Arc<Context>, cfg: Arc<RootConfig>, guild_id: Arc<GuildId>);
 }
 
-///
 /// Registers a file event listener to watch for new lines to be added
 /// to a file at a given path.
 ///
 /// # Examples
 ///
 /// ```rust
-/// let (tx, mut rx) = mpsc::channel(100);
 /// let log_tailer = LogTailer::new("/home/minecraft/server/logs/latest.log", Vec::new());
-/// tokio::spawn(async move { log_tailer.listen(tx).await });
+/// tokio::spawn(async move { log_tailer.listen(ctx.clone(), cfg.clone(), guild_id.clone()).await });
 /// ```
-///
 pub struct LogTailer {
     path: String,
     custom_keywords: Vec<String>,
@@ -49,7 +47,7 @@ impl LogTailer {
 
 #[async_trait]
 impl Listener for LogTailer {
-    async fn listen(&self, tx: Sender<MinecraftMessage>) {
+    async fn listen(&self, ctx: Arc<Context>, cfg: Arc<RootConfig>, guild_id: Arc<GuildId>) {
         info!("log_tailer:listen: using log file at '{}'", self.path);
         let parser = MessageParser::new(self.custom_keywords.clone());
 
@@ -61,38 +59,38 @@ impl Listener for LogTailer {
             .expect("Unable to add the Minecraft log file to tail");
 
         info!("log_tailer:listen: started watching the Minecraft log file");
-        let txc = tx.clone();
 
         // Wait for the next line
         while let Some(Ok(line)) = log_watcher.next().await {
-            let message = match parser.parse_line(line.line()) {
-                Some(message) => message,
-                None => continue,
-            };
+            // Check if the line is something we have to send
+            if let Some(message) = parser.parse_line(line.line()) {
+                let ctx = ctx.clone();
+                let cfg = cfg.clone();
+                let guild_id = guild_id.clone();
 
-            // Send it down the pipe
-            if let Err(e) = txc.send(message).await {
-                error!(
-                    "log_tailer:listen: unable to send message through channel: {}",
-                    e
-                );
+                // Send the message to the Discord channel
+                tokio::spawn(async move {
+                    if let Err(e) = send_to_discord(ctx, cfg, guild_id, message).await {
+                        error!(
+                            "discord:handler: unable to send a message to Discord: {}",
+                            e
+                        );
+                    }
+                });
             }
         }
     }
 }
 
-///
 /// Binds to an IP address and port to listen for messages over a network.
 /// It watches for messages at the `/message` endpoint.
 ///
 /// # Examples
 ///
 /// ```rust
-/// let (tx, mut rx) = mpsc::channel(100);
 /// let listener = Webserver::new(25585);
-/// listener.listen(tx).await;
+/// listener.listen(ctx.clone(), cfg.clone(), guild_id.clone()).await;
 /// ```
-///
 pub struct Webserver {
     port: u16,
 }
@@ -105,23 +103,241 @@ impl Webserver {
 
 #[async_trait]
 impl Listener for Webserver {
-    async fn listen(&self, tx: Sender<MinecraftMessage>) {
+    async fn listen(&self, ctx: Arc<Context>, cfg: Arc<RootConfig>, guild_id: Arc<GuildId>) {
         // POST /message/:msg
         let messages = warp::post()
             .and(warp::path("message"))
             .and(warp::body::content_length_limit(1024 * 16))
             .and(warp::body::json())
             .and_then(move |message: MinecraftMessage| {
-                let txc = tx.clone();
+                let ctx = ctx.clone();
+                let cfg = cfg.clone();
+                let guild_id = guild_id.clone();
+
+                // Send the message to the Discord channel
                 async move {
-                    match txc.send(message).await {
+                    match send_to_discord(ctx, cfg, guild_id, message).await {
                         Ok(()) => Ok(""),
-                        Err(_) => Err(warp::reject::reject()),
+                        Err(e) => {
+                            error!(
+                                "discord:handler: unable to send a message to Discord: {}",
+                                e
+                            );
+                            Err(warp::reject::reject())
+                        }
                     }
                 }
             });
 
         // TODO: Maybe figure out how to bind to a configurable address?
         warp::serve(messages).run(([0, 0, 0, 0], self.port)).await
+    }
+}
+
+/// Post a message to the configured Discord webhook.
+/// If the message is from a player, we will execute the
+/// webhook with that player's head as the avatar and their
+/// in-game name as the username.
+async fn post_to_webhook(
+    ctx: Arc<Context>,
+    message: MinecraftMessage,
+    url: &str,
+) -> Result<(), Error> {
+    // Split the url into the webhook id an token
+    let parts = match split_webhook_url(url) {
+        Some(parts) => parts,
+        None => return Err(Error::Other("invalid webhook url")),
+    };
+
+    // Get the webhook using the id and token
+    let webhook = ctx.http.get_webhook_with_token(parts.0, parts.1).await?;
+
+    // Get the avatar URL
+    let avatar_url = match message.source {
+        Source::Player => format!("https://minotar.net/helm/{}/256.png", message.name.clone()),
+        // TODO: Do something better than a blind unwrap() here
+        Source::Server => ctx.cache.current_user().await.avatar_url().unwrap(),
+    };
+
+    // Post to the webhook
+    if let Err(e) = webhook
+        .execute(&ctx.http, false, |w| {
+            w.avatar_url(avatar_url)
+                .username(message.name)
+                .content(message.content)
+        })
+        .await
+    {
+        return Err(Error::Discord(e));
+    }
+
+    Ok(())
+}
+
+/// Looks for instances of mentions in a message and attempts
+/// to replace that text with an actual Discord `@mention` (or
+/// `#channel` in the case of a channel).
+///
+/// It tries to match names using the full name and, in the
+/// case of users, optionally their  descriptor. This works
+/// for names that have spaces in them, and really probably
+/// anything else.
+async fn replace_mentions(ctx: Arc<Context>, guild_id: Arc<GuildId>, message: String) -> String {
+    let mut ret = message.clone();
+
+    if let Some(guild) = ctx.cache.guild(*guild_id).await {
+        let mut found_start = false;
+        let mut start = 0;
+        let mut end = 0;
+        let cloned = ret.clone();
+
+        for (i, c) in cloned.char_indices() {
+            if !found_start && (c == '@' || c == '#') {
+                found_start = true;
+                start = i;
+            } else if found_start && c == '#' {
+                end = i + 5;
+            } else if found_start && c == ' ' {
+                end = i;
+            } else if found_start && cloned.len() == i + 1 {
+                end = i + 1;
+            }
+
+            // Check to see if we have a mention
+            if found_start && end > 0 {
+                if let Some(mention) = cloned.get(start..end) {
+                    let name = &mention[1..];
+                    if let Some(member) = guild.member_named(name) {
+                        ret = ret.replace(mention, &member.mention().to_string());
+                    } else if let Some(role) = guild.role_by_name(name) {
+                        ret = ret.replace(mention, &role.mention().to_string());
+                    } else if let Some(id) = guild.channel_id_from_name(ctx.clone(), name).await {
+                        if let Some(channel) = ctx.cache.channel(id).await {
+                            ret = ret.replace(mention, &channel.mention().to_string());
+                        }
+                    } else {
+                        continue;
+                    }
+
+                    // If we got here, we found a mention, so reset everything
+                    start = 0;
+                    end = 0;
+                    found_start = false;
+                }
+            }
+        }
+    } else {
+        warn!("Unable to get the Guild from the cache: Guild not found");
+    }
+
+    ret
+}
+
+/// Send a message from a Minecraft server to a configured Discord channel, either
+/// directly as a message or via a webhook integration.
+///
+/// # Errors
+///
+/// Returns a `serenity::Error` if a message is unable to be sent to the channel or the webhook.
+async fn send_to_discord(
+    ctx: Arc<Context>,
+    cfg: Arc<RootConfig>,
+    guild_id: Arc<GuildId>,
+    message: MinecraftMessage,
+) -> Result<(), Error> {
+    debug!(
+        "dolphin:send_to_discord: received a message from a Minecraft instance: {:?}",
+        message
+    );
+
+    // Get the correct name to use
+    let name = match message.source {
+        Source::Player => message.name.clone(),
+        Source::Server => ctx.cache.current_user().await.name,
+    };
+
+    let mut content = message.content.clone();
+    if cfg.mentions_allowed() {
+        content = replace_mentions(Arc::clone(&ctx), guild_id, content).await;
+    }
+
+    let message = MinecraftMessage {
+        name: name.clone(),
+        content,
+        source: message.source,
+    };
+
+    // Check if we should use a webhook to post the message
+    if cfg.webhook_enabled() {
+        let url = &cfg.webhook_url();
+
+        post_to_webhook(Arc::clone(&ctx), message, url).await?
+    } else {
+        // Send the message to the channel
+        let final_msg = match message.source {
+            Source::Player => format!("**{}**: {}", message.name, message.content),
+            Source::Server => message.content,
+        };
+
+        if let Err(e) = ChannelId(cfg.get_channel_id()).say(&ctx, final_msg).await {
+            return Err(Error::Discord(e));
+        }
+    }
+
+    Ok(())
+}
+
+/// Use Regex to split the configured webhook URL into an ID and a token.
+fn split_webhook_url(url: &str) -> Option<(u64, &str)> {
+    // Only compile the regex once, since this is expensive
+    lazy_static! {
+        static ref WEBHOOK_REGEX: Regex =
+            Regex::new(r"^https://discord.com/api/webhooks/(?P<id>.*)/(?P<token>.*)$").unwrap();
+    }
+
+    let mut ret = None;
+
+    if let Ok(Some(captures)) = WEBHOOK_REGEX.captures(&url) {
+        if captures.len() != 3 {
+            return None;
+        }
+
+        let id = captures.name("id").unwrap().as_str();
+        if let Ok(id) = id.parse::<u64>() {
+            ret = Some((id, captures.name("token").unwrap().as_str()));
+        }
+    }
+
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::listener::split_webhook_url;
+
+    #[test]
+    fn parse_parts_from_webhook_url() {
+        // Given
+        let input = String::from("https://discord.com/api/webhooks/12345/67890");
+
+        // When/Then
+        match split_webhook_url(&input) {
+            Some((id, token)) => {
+                assert_eq!(id, 12345);
+                assert_eq!(token, String::from("67890"));
+            }
+            None => panic!("failed to parse Discord webhook url"),
+        }
+    }
+
+    #[test]
+    fn parse_non_webhook_url() {
+        // Given
+        let input = String::from("https://example.com");
+
+        // When/Then
+        if let Some(_) = split_webhook_url(&input) {
+            panic!("webhook split returned something when it should have returned None");
+        }
     }
 }
